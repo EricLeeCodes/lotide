a75294ee3e15153985536c9485b77594ae39c8c0
Eric Lee <ericleecodes@gmail.com>
2024-02-05 18:54:30 -0800

    Made an empty array equal to undefined

--- head.js
+++ head.js
@@ -19,4 +19,4 @@ assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 //An array with only one element should still yield that one element as its head
 assertEqual(head([5]), 5);
 //An empty array should yield undefined as its head
-assertEqual(head([]));
+assertEqual(head([]), undefined);
