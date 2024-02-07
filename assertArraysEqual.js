49c82e63f0a395810320b35effdbcbd2e09b4528
Eric Lee <ericleecodes@gmail.com>
2024-02-06 22:29:31 -0800

    Refactored eqArrays(arr1, arr2) and removed === true

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -12,7 +12,7 @@ const eqArrays = function(arr1, arr2) {
 };
 
 const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2) === true) {
+  if (eqArrays(arr1, arr2)) {
     console.log(`✅✅✅ CORRECT!: ${arr1} === ${arr2} ✅✅✅`);
   } else {
     console.log(`🛑🛑🛑 Please try again. ${arr1} !== ${arr2} 🛑🛑🛑`);
