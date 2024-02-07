64d5a8d60f2ac832a2d1acce1e5d7046798caf4a
Eric Lee <ericleecodes@gmail.com>
2024-02-06 21:57:21 -0800

    Refactored eqArrays to have parameters

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -15,7 +15,7 @@ const eqArrays = function(arr1, arr2) {
 };
 
 const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays) {
+  if (eqArrays(arr1, arr2)) {
     console.log(`CORRECT!: ${arr1} === ${arr2}`);
   } else {
     console.log(`Please try again. ${arr1} !== ${arr2}`);
