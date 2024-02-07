26f3f779e9d93e83eaa54870d377ccb9eb96718e
Eric Lee <ericleecodes@gmail.com>
2024-02-06 22:22:26 -0800

    Added test cases

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,23 +1,31 @@
 const eqArrays = function(arr1, arr2) {
-  let isTrue;
   if (arr1.length === arr2.length) {
     for (let i = 0; i < arr1.length; i++) {
       if (arr1[i] !== arr2[i]) {
         return false;
-      } else {
-        isTrue = true;
       }
     }
   } else {
-    isTrue = false;
+    return false;
   }
-  return isTrue;
+  return true;
 };
 
 const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
+  if (eqArrays(arr1, arr2) === true) {
     console.log(`✅✅✅ CORRECT!: ${arr1} === ${arr2} ✅✅✅`);
   } else {
     console.log(`🛑🛑🛑 Please try again. ${arr1} !== ${arr2} 🛑🛑🛑`);
   }
 };
+
+//Tests for different numbers
+assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertArraysEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+
+//Tests for different types
+assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+
+//General Test
+assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
