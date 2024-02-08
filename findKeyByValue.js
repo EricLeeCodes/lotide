e2cfe4927c2e9ae105fe27cba9d7a6a8502b32bf
Eric Lee <ericleecodes@gmail.com>
2024-02-08 11:35:19 -0800

    Implemented a function that finds the key by the inputted value

---
+++ findKeyByValue.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected} ✅✅✅`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected} 🛑🛑🛑`);
+  }
+};
+
+
+const findKeyByValue = function (object, value) {
+  for (const key in object) {
+    if (object[key]) {
+      if (object[key] === value) {
+        return key;
+      }
+    } else {
+      return "Please input a valid value";
+    }
+  }
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
